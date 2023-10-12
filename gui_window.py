977c5a6e05c0b418322b964e99b9c989c4231e48
Will Smith <sullivansmith057@gmail.com>
2023-10-12 14:17:49 -0500

    finally figured out tk's event binding syntax. started binding buttons to events. Need to organize the window's data to be private and preserved by the window instance

--- gui_window.py
+++ gui_window.py
@@ -1,125 +1,190 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import Ingredient_Class as ingredientClass
 
 
 class GuiWindow:
     def __init__(self, root_window, title_str, window_width, window_height):
-
         # INITIALIZE:
         self.root = root_window
         self.title = title_str
-        self.width = window_width
-        self.height = window_height
+        self._width = window_width
+        self._height = window_height
 
         self.root.title(title_str)
 
-        self.number_of_chemical_entries = 3
+        self._chem_entry_starting_row = 5
+        self._number_of_chemical_entries = 3
+
+        self._input_cell_width= 5
+        self._input_cell_height= 5
 
 
 
         # HEADER AND BODY: create & configure
-        _frame_header = Frame(master = self.root, bg="grey",
-                                    width = window_width, height= 27)
+        _frame_header = tk.Frame(master=self.root, bg="grey",
+                              width=window_width, height=27)
 
-        _frame_body = Frame(master = self.root, bd=10, bg= "white",
-                                 width = window_width, height = window_height - 27)
+        _frame_body = tk.Frame(master=self.root, bd=10, bg="white",
+                            width=window_width, height=window_height - 27)
 
-        _frame_header.pack(fill = X)
-        _frame_body.pack(fill = BOTH, expand= True)
+        _frame_header.pack(fill=tk.X)
+        _frame_body.pack(fill=tk.BOTH, expand=True)
 
-        _frame_body.columnconfigure(index = 0, weight = 30)
-        _frame_body.columnconfigure(index = 1, weight = 70)
-
-        _frame_body.rowconfigure(index = [0, 2], weight = 15)
-        _frame_body.rowconfigure(index= 1, weight = 70)
+        _frame_body.columnconfigure(index=1, weight=1)
+        _frame_body.rowconfigure(index=[1,2,3], weight= 1)
 
 
 
         # INPUT AND DISPLAY: create & configure
-        _frame_input = Frame(master = _frame_body, bg= "orange", width= 100, height= 100)
-        _frame_display = Frame(master = _frame_body, bg= "yellow", width= 300, height= 100)
+        _frame_input = ttk.Frame(master=_frame_body, width=100, height=100, borderwidth=10)
+        _frame_display = ttk.Frame(master=_frame_body, width=10, height=10)
 
-        _frame_input.grid(row= 1, column= 0, sticky= 'enw')
-        _frame_display.grid(row= 0, rowspan=3, column= 1, sticky= 'nesw')
+        _frame_input.grid(row=1, column=0, sticky='nw')
+        _frame_display.grid(row=0, rowspan=3, column=1, sticky='news')
 
-        row_indexes = list(range(0,10))
-        column_indexes = list(range(0,9))
-        _frame_input.rowconfigure(index = row_indexes, weight= 1)
-        _frame_input.columnconfigure(index = column_indexes, weight =1)
+        input_row_indexes = list(range(0, self._chem_entry_starting_row + self._number_of_chemical_entries + 1))
+        input_column_indexes = list(range(0, 9))
+        _frame_input.rowconfigure(index=input_row_indexes, weight=1, minsize= self._input_cell_height)
+        _frame_input.columnconfigure(index=input_column_indexes, weight=1, minsize= self._input_cell_width)
 
-        # cell_width= 40
-        # cell_height= 40
-        # row_spacers_dict= dict()
-        # for row in range(0,7 + self.number_of_chemical_entries):
-        #    for column in range(0, 8):
-        #        row_spacers_dict[(row,column)] = Frame(master=_frame_input, width= cell_width, height= cell_height, bg = 'blue',
-        #                                               borderwidth= 1, relief= 'solid')
-        #        row_spacers_dict[(row,column)].grid(row= row, column= column)
+
+
+        # DISPLAY CONTENTS:
+        columns = ('ingredient', 'chemical', 'quantity')
+        _treeview_table = ttk.Treeview(master=_frame_display, columns=columns, show='headings')
+        _treeview_table.heading("ingredient", text='Ingredient')
+        _treeview_table.heading('chemical', text="Chemical")
+        _treeview_table.heading('quantity', text="Qty")
+
+        _treeview_table.column('ingredient', stretch=tk.YES, anchor= 'w')
+        _treeview_table.column('chemical', stretch=tk.YES, anchor= 'w')
+        _treeview_table.column('quantity', width=50, stretch= tk.NO,anchor= 'e')
+
+        _treeview_table.grid(row=0, column=0, sticky='news')
+
+
 
         # INPUT CONTENTS:
-        # contextual buttons
-        _button_search = Button(master = _frame_input, text = 'Search')
-        _button_add = Button(master = _frame_input, text = 'Add')
-        _button_remove = Button(master = _frame_input, text = 'Remove')
+        self._context = "no context selected"
+        self._btn_dict = dict()
+        self._lbl_dict = dict()
+        self._ent_dict = dict()
 
-        _button_search.grid(row = 0, column = 0, columnspan = 2, sticky= 'esw')
-        _button_add.grid(row = 0, column = 2, columnspan = 2, sticky= 'esw')
-        _button_remove.grid(row = 0, column = 4, columnspan= 2, sticky= 'esw')
+        # contextual buttons
+        self._btn_dict["search"] = ttk.Button(master=_frame_input, text='Find')
+        self._btn_dict["add"] = ttk.Button(master=_frame_input, text='+')
+        self._btn_dict['remove'] = ttk.Button(master=_frame_input, text='-')
+
+        self._btn_dict["search"].grid(row=0, column=0, columnspan=2, sticky='esw')
+        self._btn_dict["add"].grid(row=0, column=2, columnspan=2, sticky='esw')
+        self._btn_dict['remove'].grid(row=0, column=4, columnspan=2, sticky='esw')
+
+        self._btn_dict["search"].bind('<Button-1>', self.enter_search_context)
+        self._btn_dict["add"].bind('<Button-1>', self.enter_add_context)
+        self._btn_dict['remove'].bind('<Button-1>', self.enter_remove_context)
 
         # labels
-        _label_ingredent = Label(master=_frame_input, text= "Ingredient:", anchor= 'sw')
-        _label_chemical = Label(master=_frame_input, text= "Chemicals:", anchor= 'sw')
-        _label_quantity = Label(master=_frame_input, text= "Qty:", anchor= 'sw')
+        self._lbl_dict['ingredient'] = ttk.Label(master=_frame_input, text="Ingredient:", anchor='sw')
+        self._lbl_dict['chemical'] = ttk.Label(master=_frame_input, text="Chemicals:", anchor='sw')
+        self._lbl_dict['quantity'] = ttk.Label(master=_frame_input, text="Qty:", anchor='sw')
 
-        _label_ingredent.grid(row=1, column=0, columnspan= 8, sticky= 'ensw', ipadx= 10)
-        _label_chemical.grid(row=4, column=0, columnspan= 6, sticky= 'ensw', ipadx= 10)
-        _label_quantity.grid(row=4, column=6, columnspan=2, sticky= 'ensw', ipadx= 10)
+        self._lbl_dict['ingredient'].grid(row=1, column=0, columnspan=8, sticky='ensw')
+        self._lbl_dict['chemical'].grid(row=4, column=0, columnspan=6, sticky='ensw')
+        self._lbl_dict['quantity'].grid(row=4, column=6, columnspan=2, sticky='ensw')
 
-        #entries
-        _entry_ingredient = Entry(master=_frame_input)
-        _entry_ingredient.grid(row= 2, column= 0, columnspan = 8, sticky= 'ew')
+        # entries
+        self._ent_dict['ingredient'] = ttk.Entry(master=_frame_input)
+        self._ent_dict['ingredient'].grid(row=2, column=0, columnspan=8, sticky='ensw')
 
-        self.chem_entry_starting_row = 5
-        chem_entry_dict= dict()
-        min_entry_dict = dict()
-        max_entry_dict = dict()
+        self._chem_entry_dict = dict()
+        self._min_entry_dict = dict()
+        self._max_entry_dict = dict()
 
-        for row in range(0,self.number_of_chemical_entries):
-            chem_entry_dict[row] = Entry(master=_frame_input)
-            min_entry_dict[row] = Entry(master=_frame_input, width= 5)
-            max_entry_dict[row] = Entry(master=_frame_input, width= 5)
+        for row in range(0, self._number_of_chemical_entries):
+            self._chem_entry_dict[row] = ttk.Entry(master=_frame_input)
+            self._min_entry_dict[row] = ttk.Entry(master=_frame_input, width=5)
+            self._max_entry_dict[row] = ttk.Entry(master=_frame_input, width=5)
 
-            chem_entry_dict[row].grid(row= self.chem_entry_starting_row + row, column= 0, columnspan=6, sticky='ew')
-            min_entry_dict[row].grid(row= self.chem_entry_starting_row + row, column= 6, sticky='ew')
-            max_entry_dict[row].grid(row= self.chem_entry_starting_row + row, column= 7, sticky='ew')
+            self._chem_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=0, columnspan=6, sticky='ensw')
+            self._min_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=6, sticky='ensw')
+            self._max_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=7, sticky='ensw')
 
         # edit chemical entries buttons
-        _button_add_chemical = Button(master= _frame_input, text='+')
-        _button_remove_chemical = Button(master= _frame_input, text='-')
+        self._btn_dict["increase_capacity"] = ttk.Button(master=_frame_input, text='+')
+        self._btn_dict["decrease_capacity"] = ttk.Button(master=_frame_input, text='-')
 
-        _button_add_chemical.grid(row=self.chem_entry_starting_row + self.number_of_chemical_entries, column=0, sticky='ew')
-        _button_remove_chemical.grid(row=self.chem_entry_starting_row + self.number_of_chemical_entries, column=1, sticky='ew')
+        self._btn_dict["increase_capacity"].grid(row=self._chem_entry_starting_row + self._number_of_chemical_entries,
+                                                 column=0, sticky='ew')
+        self._btn_dict["decrease_capacity"].grid(row=self._chem_entry_starting_row + self._number_of_chemical_entries,
+                                                 column=1, sticky='ew')
 
         # Submission Buttons
-        _button_confirm = Button(master= _frame_input, text= '>>')
-        _button_clear = Button(master= _frame_input, text= 'C')
+        self._btn_dict["submit"] = ttk.Button(master=_frame_input, text='>>')
+        self._btn_dict["clear"] = ttk.Button(master=_frame_input, text='C')
 
-        offset= self.chem_entry_starting_row+ self.number_of_chemical_entries + 1
-        _button_clear.grid(row= offset, column=6, sticky= 'senw')
-        _button_confirm.grid(row= offset, column= 7, sticky= 'snwe')
-
-
+        offset = self._chem_entry_starting_row + self._number_of_chemical_entries + 1
+        self._btn_dict["clear"].grid(row=offset, column=6, sticky='senw')
+        self._btn_dict["submit"].grid(row=offset, column=7, sticky='snwe')
 
 
-
+        self.root.mainloop()
 
 
 
-        # labels
 
 
+    def enter_search_context(self, event):
+        if self._context != 'search':
+            self._context = 'search'
+            self._btn_dict["search"].state(['disabled'])
+            self._btn_dict["add"].state(['!disabled'])
+            self._btn_dict["remove"].state(['!disabled'])
+            # print("context changed to " + self._context)
 
-        # DISPLAY WINDOW
-        mainloop()
+
+    def enter_add_context(self, event):
+        if self._context != 'add':
+            self._context = 'add'
+            self._btn_dict["search"].state(['!disabled'])
+            self._btn_dict["add"].state(['disabled'])
+            self._btn_dict["remove"].state(['!disabled'])
+            # print("context changed to " + self._context)
 
 
+    def enter_remove_context(self, event):
+        if self._context != 'remove':
+            self._context = 'remove'
+            self._btn_dict["search"].state(['!disabled'])
+            self._btn_dict["add"].state(['!disabled'])
+            self._btn_dict["remove"].state(['disabled'])
+            # print("context changed to " + self._context)
 
+
+    def increment_chemical_entries(self, event):
+        pass
+
+
+    def decrement_chemical_entries(self, event):
+        pass
+
+    def clear_entries(self,event):
+        pass
+
+    def submit_query(self, event):
+        pass
+
+    def search_entry(self, search_tuple: tuple):
+        pass
+
+    def add_entry(self, add_tuple: tuple):
+        pass
+
+    def remove_entry(self, remove_tuple: tuple):
+        pass
+
+    def populate_display_with_entries(self, matches_list: list):
+        pass
+
+    def convert_ingredient_to_tuple(self, ingredient: ingredientClass.Ingredient):
+        pass
--- main.py
+++ main.py
@@ -2,28 +2,8 @@ from Utilities import *
 from gui_window import *
 
 
-def old_main():
-    print("Ingredient Tracker Application Started. Welcome!")
-    print("displaying gui window")
-
-    print("Initializing ingredient data...")
-    ingredient_set = set()
-    is_application_running = True
-    while is_application_running:
-        main_menu_input = get_main_menu_selection()
-        if main_menu_input == 'q':
-            is_application_running = False
-            continue
-        elif main_menu_input == '1':
-            control_ingredient_interface(ingredient_set)
-        else:
-            print('Option \'' + main_menu_input + '\' isn\'t a valid selection')
-
-    print("Ingredient Tracker Application Closing. Thank you!")
-
-
 def main():
-    root = Tk()
+    root = tk.Tk()
     title_str = "Ingredient Tracker"
     window_width = 1000
     window_height = 700
@@ -32,3 +12,4 @@ def main():
 
 
 main()
+
