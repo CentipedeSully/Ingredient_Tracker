e300b14f9d3d86b9c7f0c509f628b94737f5a170
Will Smith <sullivansmith057@gmail.com>
2023-10-13 11:46:08 -0500

    adjusted the input frame to have a more scalable amount of chemical entries. Also added functionality to the increase/decrease chemical entries buttons. They now work as expected.

--- gui_window.py
+++ gui_window.py
@@ -13,9 +13,8 @@ class GuiWindow:
 
         self.root.title(title_str)
 
-        self._chem_entry_starting_row = 5
         self._number_of_chemical_entries = 3
-
+        self._max_number_of_chemicals = 16
         self._input_cell_width= 5
         self._input_cell_height= 5
 
@@ -44,10 +43,20 @@ class GuiWindow:
         self._frame_dict['input'].grid(row=1, column=0, sticky='nw')
         self._frame_dict['table'].grid(row=0, rowspan=3, column=1, sticky='news')
 
-        self._input_row_indexes = list(range(0, self._chem_entry_starting_row + self._number_of_chemical_entries + 1))
+        self._input_row_indexes = list(range(0, 8))
         self._input_column_indexes = list(range(0, 9))
         self._frame_dict['input'].rowconfigure(index=self._input_row_indexes, weight=1, minsize= self._input_cell_height)
         self._frame_dict['input'].columnconfigure(index=self._input_column_indexes, weight=1, minsize= self._input_cell_width)
+
+        # row index
+        # 0: Context Buttons
+        # 1: Ingredient Name Label
+        # 2: Ingredient Entry
+        # 3: Space
+        # 4: Chemicals Label
+        # 5: Chemical Entries Frame
+        # 6: Add/Remove entry Buttons
+        # 7: Clear/Submit buttons
 
 
 
@@ -102,31 +111,27 @@ class GuiWindow:
         self._min_entry_dict = dict()
         self._max_entry_dict = dict()
 
-        for row in range(0, self._number_of_chemical_entries):
-            self._chem_entry_dict[row] = ttk.Entry(master=self._frame_dict['input'])
-            self._min_entry_dict[row] = ttk.Entry(master=self._frame_dict['input'], width=5)
-            self._max_entry_dict[row] = ttk.Entry(master=self._frame_dict['input'], width=5)
-
-            self._chem_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=0, columnspan=6, sticky='ensw')
-            self._min_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=6, sticky='ensw')
-            self._max_entry_dict[row].grid(row=self._chem_entry_starting_row + row, column=7, sticky='ensw')
+        self._frame_dict['chemical_inputs'] = ttk.Frame(master= self._frame_dict['input'])
+        self._frame_dict['chemical_inputs'].columnconfigure(index=  self._input_column_indexes, weight=1, minsize= self._input_cell_width)
+        self._frame_dict['chemical_inputs'].grid(row= 5, column=0, columnspan=7, sticky= 'nesw')
+        self.rebuild_chemical_entries(self._number_of_chemical_entries)
 
         # edit chemical entries buttons
         self._btn_dict["increase_capacity"] = ttk.Button(master=self._frame_dict['input'], text='+')
         self._btn_dict["decrease_capacity"] = ttk.Button(master=self._frame_dict['input'], text='-')
 
-        self._btn_dict["increase_capacity"].grid(row=self._chem_entry_starting_row + self._number_of_chemical_entries,
-                                                 column=0, sticky='ew')
-        self._btn_dict["decrease_capacity"].grid(row=self._chem_entry_starting_row + self._number_of_chemical_entries,
-                                                 column=1, sticky='ew')
+        self._btn_dict["increase_capacity"].grid(row=6, column=0, sticky='ew')
+        self._btn_dict["decrease_capacity"].grid(row=6, column=1, sticky='ew')
+
+        self._btn_dict["increase_capacity"].bind('<Button-1>', self.increment_chemical_entries)
+        self._btn_dict["decrease_capacity"].bind('<Button-1>', self.decrement_chemical_entries)
 
         # Submission Buttons
         self._btn_dict["submit"] = ttk.Button(master=self._frame_dict['input'], text='>>')
         self._btn_dict["clear"] = ttk.Button(master=self._frame_dict['input'], text='C')
 
-        self._offset_from_chemical_entries = self._chem_entry_starting_row + self._number_of_chemical_entries + 1
-        self._btn_dict["clear"].grid(row=self._offset_from_chemical_entries, column=6, sticky='senw')
-        self._btn_dict["submit"].grid(row=self._offset_from_chemical_entries, column=7, sticky='snwe')
+        self._btn_dict["clear"].grid(row=7, column=6, sticky='senw')
+        self._btn_dict["submit"].grid(row=7, column=7, sticky='snwe')
 
 
         self.root.mainloop()
@@ -162,12 +167,57 @@ class GuiWindow:
             # print("context changed to " + self._context)
 
 
+    def rebuild_chemical_entries(self, number_of_desired_entries: int):
+        number_of_desired_entries = max(0, number_of_desired_entries)
+
+        # remove current entries
+        if len(self._chem_entry_dict) > 0:
+            print("deleting " + str(len(self._chem_entry_dict)) + ' chem entries...')
+            for row in range(0, len(self._chem_entry_dict)):
+                self._chem_entry_dict[row].grid_forget()
+                self._min_entry_dict[row].grid_forget()
+                self._max_entry_dict[row].grid_forget()
+
+                del(self._chem_entry_dict[row])
+                del(self._min_entry_dict[row])
+                del(self._max_entry_dict[row])
+
+            print("old chemical entries deleted. Current chem_dict size: " + str(len(self._chem_entry_dict)))
+
+        # reconfigure chem entry frame
+        if number_of_desired_entries > 1:
+            index_list= list(range(0,number_of_desired_entries))
+            self._frame_dict['chemical_inputs'].rowconfigure(index= index_list, weight=1, minsize=self._input_cell_width)
+        else:
+            self._frame_dict['chemical_inputs'].rowconfigure(index=0, weight=1, minsize=self._input_cell_width)
+
+        # create new entries
+        for row in range(0, number_of_desired_entries):
+            self._chem_entry_dict[row] = ttk.Entry(master=self._frame_dict['chemical_inputs'])
+            self._min_entry_dict[row] = ttk.Entry(master=self._frame_dict['chemical_inputs'], width=5)
+            self._max_entry_dict[row] = ttk.Entry(master=self._frame_dict['chemical_inputs'], width=5)
+
+            self._chem_entry_dict[row].grid(row=row, column=0, columnspan=6, sticky='ensw')
+            self._min_entry_dict[row].grid(row=row, column=6, sticky='ensw')
+            self._max_entry_dict[row].grid(row=row, column=7, sticky='ensw')
+
+
+        # update utils
+        self._number_of_chemical_entries = number_of_desired_entries
+
+
+
+
     def increment_chemical_entries(self, event):
-        pass
+        if self._number_of_chemical_entries < self._max_number_of_chemicals:
+            self.rebuild_chemical_entries(self._number_of_chemical_entries + 1)
 
 
     def decrement_chemical_entries(self, event):
-        pass
+        if self._number_of_chemical_entries > 0:
+            self.rebuild_chemical_entries(self._number_of_chemical_entries - 1)
+
+
 
     def clear_entries(self,event):
         pass
@@ -187,6 +237,9 @@ class GuiWindow:
     def populate_display_with_entries(self, matches_list: list):
         pass
 
+
+    def remove_widget_from_grid(self, widget):
+        self._btn_dict['search'].grid_remove()
 
     @staticmethod
     def convert_ingredient_to_entry_list(ingredient: ingre.Ingredient) -> list:
